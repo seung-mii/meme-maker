20874c10e80da7c4ab164008b02feb34874e1d23
withsme <tmdal9953@gmail.com>
2023-02-20 00:04:52 +0900

    feat : 이미지 및 텍스트 추가, 파일 저장 기능

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const modeBtn = document.getElementById("mode-btn");
-const destroyBtn = document.getElementById("destroy-btn");
+const saveBtn = document.getElementById("save");
+const textInput = document.getElementById("text");
+const fileInput = document.getElementById("file");
 const eraserBtn = document.getElementById("eraser-btn");
+const destroyBtn = document.getElementById("destroy-btn");
+const modeBtn = document.getElementById("mode-btn");
 const colorOptions = Array.from(
   document.getElementsByClassName("color-option")
 );
@@ -18,6 +21,7 @@ const CANVAS_HEIGHT = 800;
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 
+ctx.lineCap = "round"; // 선 끝을 둥글게 설정
 ctx.lineWidth = lineWidth.value;
 // lineWidth의 기본값을 JS에서도 설정
 // 딱 한 번만 실행됨
@@ -87,7 +91,47 @@ function onEraserClick() { // 지우는 것은 하얀색으로 선 긋는 것과
   modeBtn.innerText = "Fill";
 }
 
+function onFileChange(event) {
+  const file = event.target.files[0]; // 파일 배열 중 첫 번째 파일을 불러옴
+  const url = URL.createObjectURL(file);
+  // 유저가 업로드한 이미지로부터 브라우저가 만든 브라우저에서만 사용 가능한 주소
+  const image = new Image();
+  image.src = url;
+  // <img src="" /> 와 같은 거임 
+  image.onload = function () { // image.onload : 이미지가 로드되었을 때    
+    // = image.addEventListener("onload", function);
+    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    fileInput.value = null; // 계속 다른 파일을 추가할 수 있도록 
+  };
+}
+
+function onDoubleClick(event) {
+  const text = textInput.value;
+  if (text !== "") { // text에 값이 있다면
+    ctx.save(); // .save : ctx의 현재 상태, 색상, 스타일 등 모든 것을 저장
+    ctx.lineWidth = 1; // 글씨체 굵기를 1로 안하면 글씨가 제대로 안보임 
+    ctx.font = "68px sans-serif";
+    ctx.fillText(text, event.offsetX, event.offsetY); // 글씨체 내부가 채워서 출력됨 
+    ctx.restore(); // .restore : 이전에 저장된 상태로 돌아감
+    // 수정이 완료되면 .restore 사용
+    // .save와 .restore 사이의 수정은 저장되지 않음
+  }
+}
+
+function onSaveClick() {
+  const url = canvas.toDataURL();
+  // 내가 만든 이미지가 url로 인코딩되어 결과로 나옴
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "myDrawing.png"; // myDrawing.png 와 같은 이름으로 이미지를 저장 
+  a.click(); // 저장하기 위해 a 태그를 임의로 클릭
+  // <a href = "" download = ""> </a> 와 같음
+}
+
+canvas.addEventListener("dblclick", onDoubleClick);
+// dblclick은 마우스가 더블 클릭한 상태
 canvas.addEventListener("mousemove", onMove);
+// mousemove는 마우스가 움직일 때마다 이벤트가 발생
 canvas.addEventListener("mousedown", startPainting);
 // mousedown은 마우스가 눌러지고 있는 상태
 canvas.addEventListener("mouseup", cancelPainting);
@@ -101,3 +145,6 @@ colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
 modeBtn.addEventListener("click", onModeClick);
 destroyBtn.addEventListener("click", onDestroyClick);
 eraserBtn.addEventListener("click", onEraserClick);
+fileInput.addEventListener("change", onFileChange);
+// 파일에 변화가 생기면 이벤트 발생
+saveBtn.addEventListener("click", onSaveClick);
